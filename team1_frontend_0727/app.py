d4bd28c53eb9e9e33af0358d5a68801cb5fb8d93
kiwi248 <ykw492@gmail.com>
2026-07-27 14:33:16 +0900

    1433

--- team1_frontend_0727/app.py
+++ team1_frontend_0727/app.py
@@ -7,3 +7,24 @@ st.set_page_config(
     page_icon="😀",
     layout="wide",
 )
+
+home_page = st.Page("app_pages/01_home.py", default=True)
+login_page = st.Page("app_pages/00_login.py")
+signup_page = st.Page("app_pages/02_signup.py")
+
+
+pages = [home_page, login_page, signup_page]
+
+
+
+navigation = st.navigation(pages, position="hidden")
+
+with st.sidebar:
+    st.info("화면왼쪽")
+    st.divider()
+    st.page_link(home_page, label="😀 HOME")
+    st.page_link(login_page, label="😀 Sign In")
+    st.page_link(signup_page, label="😀 Sign Up")
+
+
+navigation.run()
